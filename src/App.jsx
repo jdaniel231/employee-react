97a1bda0d84f7632f6c214e2f58ccccfe1208b5d
jdaniel231 <jdaniel231@gmail.com>
2024-08-05 00:04:23 -0300

    dashboar e route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './Components/Dashboard';
+import Home from './Components/Home';
+import Employee from './Components/Employee.jsx';
+import Profile from './Components/Profile.jsx';
+import Category from './Components/Category.jsx';
 
 function App() {
 
@@ -10,7 +14,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/adminlogin" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<Home />}></Route>
+          <Route path='employee' element={<Employee />}></Route>
+          <Route path='category' element={<Category />}></Route>
+          <Route path='profile' element={<Profile />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,7 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Dashboard = () => {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
     <div className="d-flex">
       <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{ width: "280px", height: "100vh" }}>
@@ -12,25 +15,25 @@ const Dashboard = () => {
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
           <li className="nav-item">
-            <Link to="/dashboard" className="nav-link active text-white">
+            <Link to="/dashboard" className={`nav-link ${pathname === '/dashboard' ? 'active' : 'text-white'}`}>
               <i className="bi bi-house-door me-2"></i>
               Dashboard
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/dashboard" className="nav-link text-white">
+            <Link to="/dashboard/employee" className={`nav-link ${pathname === '/dashboard/employee' ? 'active' : 'text-white'}`}>
               <i className="bi bi-people-fill me-2"></i>
               Manage Employees
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/orders" className="nav-link text-white">
+            <Link to="/dashboard/category" className={`nav-link ${pathname === '/dashboard/category' ? 'active' : 'text-white'}`}>
               <i className="bi bi-window-dock me-2"></i>
               Category
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/products" className="nav-link text-white">
+            <Link to="/dashboard/profile" className={`nav-link ${pathname === '/dashboard/profile' ? 'active' : 'text-white'}`}>
               <i className="bi bi-person me-2"></i>
               Profile
             </Link>
@@ -45,8 +48,9 @@ const Dashboard = () => {
       </div>
       <div className="p-0 col m-0" style={{ flexGrow: 1 }}>
         <div className='p-2 d-flex justify-content-center shadow'>
-          <h4>Employee Management system</h4>
+          <h4>Employee Management System</h4>
         </div>
+        <Outlet />
       </div>
     </div>
   );
